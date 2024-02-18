d90d78062d93744c8f1a082e3c6711c28d77185b
Clément <clement2104.boillot@gmail.com>
2024-02-18 03:07:48 +0100

    Sort messages array

--- src/messages/messages.h
+++ src/messages/messages.h
@@ -14,61 +14,61 @@
 void client_process_message(client_t *client, char *buffer);
 bool client_logged(client_t *client);
 
-void msg_user(client_t *client, const char *buffer);
-void msg_pass(client_t *client, const char *buffer);
-void msg_syst(client_t *client, const char *buffer);
-void msg_quit(client_t *client, const char *buffer);
-void msg_noop(client_t *client, const char *buffer);
-void msg_cwd(client_t *client, const char *buffer);
 void msg_cdup(client_t *client, const char *buffer);
-void msg_pwd(client_t *client, const char *buffer);
-void msg_xpwd(client_t *client, const char *buffer);
+void msg_cwd(client_t *client, const char *buffer);
+void msg_dele(client_t *client, const char *buffer);
 void msg_help(client_t *client, const char *buffer);
 void msg_mkd(client_t *client, const char *buffer);
+void msg_noop(client_t *client, const char *buffer);
+void msg_pass(client_t *client, const char *buffer);
+void msg_pwd(client_t *client, const char *buffer);
+void msg_quit(client_t *client, const char *buffer);
 void msg_rmd(client_t *client, const char *buffer);
-void msg_dele(client_t *client, const char *buffer);
+void msg_syst(client_t *client, const char *buffer);
+void msg_user(client_t *client, const char *buffer);
+void msg_xpwd(client_t *client, const char *buffer);
 
 static const struct msg_s {
     char *cmd;
     void (*func)(client_t *client, const char *buffer);
     char *help;
 } INCOMMING_MSG[] = {
+    {"ABOR", NULL, NULL},
     {"ACCT", NULL, NULL},
-    {"USER", msg_user, "Specify user for authentication"},
-    {"PASS", msg_pass, "Specify password for authentication"},
-    {"CWD", msg_cwd, "Change working directory"},
+    {"ALLO", NULL, NULL},
+    {"APPE", NULL, NULL},
     {"CDUP", msg_cdup, "Change working directory to parent directory"},
-    {"SMNT", NULL, NULL},
+    {"CWD", msg_cwd, "Change working directory"},
+    {"DELE", msg_dele, "Delete file on the server"},
+    {"HELP", msg_help, "List available commands"},
+    {"LIST", NULL, "List files in the current working directory"},
+    {"LPRT", NULL, NULL},
+    {"MKD", msg_mkd, "Make directory"},
+    {"MODE", NULL, NULL},
+    {"NLST", NULL, NULL},
+    {"NOOP", msg_noop, "Do nothing"},
+    {"PASS", msg_pass, "Specify password for authentication"},
+    {"PASV", NULL, "Enable \"passive\" mode for data transfer"},
+    {"PORT", NULL, "Enable \"active\" mode for data transfer"},
+    {"PWD", msg_pwd, "Print working directory"},
     {"QUIT", msg_quit, "Disconnection"},
     {"REIN", NULL, NULL},
-    {"PORT", NULL, "Enable \"active\" mode for data transfer"},
-    {"PASV", NULL, "Enable \"passive\" mode for data transfer"},
-    {"TYPE", NULL, NULL},
-    {"STRU", NULL, NULL},
-    {"MODE", NULL, NULL},
-    {"RETR", NULL, "Download file from server to client"},
-    {"STOR", NULL, "Upload file from client to server"},
-    {"STOU", NULL, NULL},
-    {"APPE", NULL, NULL},
-    {"ALLO", NULL, NULL},
     {"REST", NULL, NULL},
+    {"RETR", NULL, "Download file from server to client"},
+    {"RMD", msg_rmd, "Remove directory"},
     {"RNFR", NULL, NULL},
     {"RNTO", NULL, NULL},
-    {"ABOR", NULL, NULL},
-    {"DELE", msg_dele, "Delete file on the server"},
-    {"RMD", msg_rmd, "Remove directory"},
-    {"MKD", msg_mkd, "Make directory"},
-    {"PWD", msg_pwd, "Print working directory"},
-    {"LIST", NULL, "List files in the current working directory"},
-    {"NLST", NULL, NULL},
     {"SITE", NULL, NULL},
-    {"SYST", msg_syst, "Print system information"},
+    {"SMNT", NULL, NULL},
     {"STAT", NULL, NULL},
-    {"HELP", msg_help, "List available commands"},
-    {"NOOP", msg_noop, "Do nothing"},
-    {"LPRT", NULL, NULL},
-    {"XMKD", NULL, NULL},
-    {"XRMD", NULL, NULL},
-    {"XPWD", msg_xpwd, "Print working directory"},
+    {"STOR", NULL, "Upload file from client to server"},
+    {"STOU", NULL, NULL},
+    {"STRU", NULL, NULL},
+    {"SYST", msg_syst, "Print system information"},
+    {"TYPE", NULL, NULL},
+    {"USER", msg_user, "Specify user for authentication"},
     {"XCUP", NULL, NULL},
+    {"XMKD", NULL, NULL},
+    {"XPWD", msg_xpwd, "Print working directory"},
+    {"XRMD", NULL, NULL},
 };
