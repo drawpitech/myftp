de379bf85a6c081ba03181df2fd618c17d9482c5
Clément <clement2104.boillot@gmail.com>
2024-02-26 16:37:48 +0100

    Use 0 in passive mode to select a random port

--- src/messages/passive.c
+++ src/messages/passive.c
@@ -13,34 +13,24 @@
 #include "debug.h"
 #include "messages.h"
 
-static const unsigned short MIN_PORT = 49152;
-static const unsigned short MAX_PORT = 65535;
-
-static bool try_port(client_t *client, unsigned short port)
+static bool get_port(client_t *client)
 {
     socket_t *sock = &client->data_socket;
+    socklen_t len = sizeof(sock->sock_in);
 
-    DEBUG("try port: %d", port);
-    sock->sock_in = client->socket.sock_in;
-    sock->sock_in.sin_port = htons(port);
-    if (bind(
-            sock->fd, (struct sockaddr *)&sock->sock_in,
-            sizeof(sock->sock_in)) == -1 ||
-        listen(sock->fd, LISTEN_BACKLOG) != 0)
-        return false;
-    return true;
-}
-
-static unsigned short get_port(client_t *client)
-{
     client->data_socket.fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client->data_socket.fd == -1)
         return 0;
-    for (unsigned short i = MIN_PORT; i != MAX_PORT; i++)
-        if (try_port(client, i))
-            return i;
+    sock->sock_in = client->socket.sock_in;
+    sock->sock_in.sin_port = 0;
+    if (bind(
+            sock->fd, (struct sockaddr *)&sock->sock_in,
+            sizeof(sock->sock_in)) == 0 &&
+        listen(sock->fd, LISTEN_BACKLOG) == 0 &&
+        getsockname(sock->fd, (struct sockaddr *)&sock->sock_in, &len) == 0)
+        return true;
     close(client->data_socket.fd);
-    return 0;
+    return false;
 }
 
 void msg_pasv(client_t *client, UNUSED const char *buffer)
@@ -52,11 +42,11 @@ void msg_pasv(client_t *client, UNUSED const char *buffer)
     if (client == NULL || !client_logged(client))
         return;
     client->state = NO_DATA_SOCK;
-    port = get_port(client);
-    if (port == 0) {
+    if (!get_port(client)) {
         client_write(client, MSG_550);
         return;
     }
+    port = ntohs(client->data_socket.sock_in.sin_port);
     DEBUG("passive mode: port: %d\n", port);
     client->state = PASSIVE_MODE;
     raw_ip = htonl(client->socket.sock_in.sin_addr.s_addr);
